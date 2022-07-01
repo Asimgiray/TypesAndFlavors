e4d0612affb0878c98893f6249f3239e6bb65bec
cci-Giray <asimgiray.boyraz@cci.com.tr>
2022-07-01 19:19:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/flavor_variant/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.flavor_variant
+
+const val BASE_URL = BuildConfig.BASE_URL
+const val BUILD_TYPE = BuildConfig.BUILD_TYPE
+const val FLAVOR = BuildConfig.FLAVOR_TYPE
+
+val ENABLE_LOG = BuildConfig.ENABLE_LOG
---
+++ app/src/main/java/com/example/flavor_variant/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.flavor_variant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
